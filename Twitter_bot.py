1b5d45dabd2519eb9ac1ca3226b8a402533b9e00
Nichoals Read <Read1762@gmail.com>
2019-12-12 17:32:39 -0500

    adding an interval to tweet out everyday

--- Twitter_bot.py
+++ Twitter_bot.py
@@ -1,6 +1,7 @@
 from github import Github
 from datetime import datetime
 import tweepy
+import time
 from tweepy import OAuthHandler
 import keys
 # First create a Github instance:
@@ -9,6 +10,7 @@ ckey = keys.ckey
 csecret = keys.csecret
 atoken = keys.atoken
 asecret = keys.asecret
+INTERVAL = 60 * 60 * 12
 
 auth = OAuthHandler(ckey, csecret)
 auth.set_access_token(atoken, asecret)
@@ -19,6 +21,8 @@ api = tweepy.API(auth)
 g = Github(keys.username, keys.password)
 days_updated= []
 # Then play with your Github objects:
-for repo in g.get_user().get_repos():
-    if d.date() == repo.updated_at.date():
-        api.update_status(f'yes he did, he updated: {repo.name}')
+while True:
+	for repo in g.get_user().get_repos():
+		if d.date() == repo.updated_at.date():
+			api.update_status(f'yes he did, he updated: {repo.name}')
+	time.sleep(INTERVAL)
