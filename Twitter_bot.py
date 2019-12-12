15eddd523831c8c77ac6d204331f322fb3fedd22
Nichoals Read <Read1762@gmail.com>
2019-12-12 17:49:02 -0500

    changing the keys

--- Twitter_bot.py
+++ Twitter_bot.py
@@ -4,12 +4,13 @@ import tweepy
 import time
 from tweepy import OAuthHandler
 import keys
+from os import environ
 # First create a Github instance:
 d = datetime.today()
-ckey = keys.ckey
-csecret = keys.csecret
-atoken = keys.atoken
-asecret = keys.asecret
+CONSUMER_KEY = environ['CONSUMER_KEY']
+CONSUMER_SECRET = environ['CONSUMER_SECRET']
+ACCESS_KEY = environ['ACCESS_KEY']
+ACCESS_SECRET = environ['ACCESS_SECRET']
 INTERVAL = 60 * 60 * 12
 
 auth = OAuthHandler(ckey, csecret)
@@ -18,7 +19,7 @@ api = tweepy.API(auth)
 
 
 # using username and password
-g = Github(keys.username, keys.password)
+g = Github(username, password)
 days_updated= []
 # Then play with your Github objects:
 while True:
