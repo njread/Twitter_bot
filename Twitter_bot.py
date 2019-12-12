72ec1ee106188a3abf9e7ef96556ca2650e9b470
Nichoals Read <Read1762@gmail.com>
2019-12-12 18:17:20 -0500

    forcing the correct versions

--- Twitter_bot.py
+++ Twitter_bot.py
@@ -1,17 +1,15 @@
 from github import Github
 from datetime import datetime
 import tweepy
-import time
 from tweepy import OAuthHandler
 import keys
-from os import environ
 # First create a Github instance:
 d = datetime.today()
-CONSUMER_KEY = environ['CONSUMER_KEY']
-CONSUMER_SECRET = environ['CONSUMER_SECRET']
-ACCESS_KEY = environ['ACCESS_KEY']
-ACCESS_SECRET = environ['ACCESS_SECRET']
-INTERVAL = 60 * 60 * 12
+ckey = keys.ckey
+csecret = keys.csecret
+atoken = keys.atoken
+asecret = keys.asecret
+INTERVAL = 60 * 60 * 6
 
 auth = OAuthHandler(ckey, csecret)
 auth.set_access_token(atoken, asecret)
@@ -19,11 +17,9 @@ api = tweepy.API(auth)
 
 
 # using username and password
-g = Github(username, password)
+g = Github(keys.username, keys.password)
 days_updated= []
 # Then play with your Github objects:
-while True:
-	for repo in g.get_user().get_repos():
-		if d.date() == repo.updated_at.date():
-			api.update_status(f'yes he did, he updated: {repo.name}')
-	time.sleep(INTERVAL)
+for repo in g.get_user().get_repos():
+    if d.date() == repo.updated_at.date():
+        api.update_status(f'yes he did, he updated: {repo.name}')
