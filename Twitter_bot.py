5398d542684c695b6833501db8e0d783bc96af86
Nichoals Read <Read1762@gmail.com>
2019-12-17 15:04:51 -0500

    makeing the twitter bot better

--- Twitter_bot.py
+++ Twitter_bot.py
@@ -27,9 +27,10 @@ count = 0
 # while loop that runs indefinitely that check if my dates.
 while True:
 	for repo in g.get_user().get_repos():
-		if d.date() == repo.updated_at.date():
-			api.update_status(f'yes he did, he updated: {repo.name}')
-			count +=1
-		else:
-			api.update_status(f"No he didn't he broke a {count} day long streak")
+		days_updated.append(repo.updated_at.date())
+	if d.date() in repo.updated_at.date():
+		api.update_status(f'yes he did, he updated: {repo.name}')
+		count +=1
+	else:
+		api.update_status(f"No he didn't he broke a {count} day long streak")
 	time.sleep(INTERVAL)
