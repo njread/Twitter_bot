882948db9e145c607cab5395284f87638d4f81bd
Nichoals Read <Read1762@gmail.com>
2019-12-17 14:27:21 -0500

    made a changes to the twitter bot now it keeps track of the the number of days and tweets out if I dindt tweet and how lond the streak I broke was

--- Twitter_bot.py
+++ Twitter_bot.py
@@ -23,9 +23,13 @@ api = tweepy.API(auth)
 # using username and password
 g = Github(username, password)
 days_updated= []
+count = 0
 # while loop that runs indefinitely that check if my dates.
 while True:
 	for repo in g.get_user().get_repos():
 		if d.date() == repo.updated_at.date():
 			api.update_status(f'yes he did, he updated: {repo.name}')
+			count +=1
+		else:
+			api.update_status(f"No he didn't he broke a {count} day long streak")
 	time.sleep(INTERVAL)
