cdfb91e2e8f13adbfca2628afaa0a4487656bc0d
Nichoals Read <Read1762@gmail.com>
2019-12-18 15:52:12 -0500

    adding out put for the heroku log

--- Twitter_bot.py
+++ Twitter_bot.py
@@ -14,13 +14,11 @@ ACCESS_SECRET = environ['ACCESS_SECRET']
 username = environ['username']
 password = environ['password']
 INTERVAL = 60 * 60 * 24
-
+# twitter api authentication 
 auth = OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
-
-
-# using username and password
+# git hub username and password
 g = Github(username, password)
 days_updated= []
 count = 0
@@ -30,7 +28,9 @@ while True:
 		days_updated.append(repo.updated_at.date())
 	if d.date() in days_updated:
 		api.update_status(f'yes he did, he updated: {repo.name}')
+		print('I tweeted that he pushed')
 		count +=1
 	else:
 		api.update_status(f"No he didn't he broke a {count} day long streak")
+		print('I tweeted that he did not push')
 	time.sleep(INTERVAL)
