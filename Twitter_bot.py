09fc8b9858d6400d9fb822b697203c9ddc7500dd
Nichoals Read <Read1762@gmail.com>
2019-12-21 17:22:00 -0500

    added try except for some error handeling

--- Twitter_bot.py
+++ Twitter_bot.py
@@ -4,6 +4,7 @@ import tweepy
 from tweepy import OAuthHandler
 from os import environ
 import time
+from random import randrange
 
 d = datetime.today()
 
@@ -13,7 +14,7 @@ ACCESS_KEY = environ['ACCESS_KEY']
 ACCESS_SECRET = environ['ACCESS_SECRET']
 username = environ['username']
 password = environ['password']
-INTERVAL = 60 * 60 * 24
+INTERVAL = 10
 # twitter api authentication 
 auth = OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
@@ -24,14 +25,24 @@ g = Github(username, password)
 # while loop that runs indefinitely that check if my dates.
 while True:
 	days_updated= []
-	count = 0
+	didnt_push = ['No he didnt he should feel bad!!',
+				  'No he messed up and forgot!!',
+				  'Bro come on no pushes??',
+				  'He is trying his best but he got tied up so no pushes today'
+				  ]
 	for repo in g.get_user().get_repos():
 		days_updated.append(repo.updated_at.date())
-	if d.date() in days_updated:
-		api.update_status(f'yes he did, he updated: {repo.name}')
-		print('I tweeted that he pushed')
-		count +=1
-	else:
-		api.update_status(f"No he didn't he broke a {count} day long streak")
-		print('I tweeted that he did not push')
-	time.sleep(INTERVAL)
+	try:
+		if d.date() in days_updated:
+			api.update_status(f'yes he did, he updated: {repo.name}')
+			print('I tweeted that he pushed')
+			count +=1
+		else:
+			api.update_status(f'{didnt_push[randrange(3)]}')
+			print(f'I tweeted that he did not push i tweeted out response: {randrange(3)}')
+		time.sleep(INTERVAL)
+	except:
+		print('error duplicated tweet deployed back up tweet')
+		time.sleep(INTERVAL)
+		api.update_status(f'{didnt_push[randrange(3)]} he probably got on error on line {randrange(1000)} and got stuck')
+		pass
